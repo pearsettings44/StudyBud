0db4043c742cfa20ab6ae0d3fc4a187550a04d76
pearsettings44 <jose.a.pereira03@gmail.com>
2022-08-04 12:42:03 +0100

    add db

--- base/views.py
+++ base/views.py
@@ -1,3 +1,23 @@
+import imp
 from django.shortcuts import render
-
+from .models import Room
 # Create your views here.
+
+
+rooms = [
+    {'id': '1', 'name': 'Lets learn python'},
+    {'id': '2', 'name': 'Lets learn django'},
+    {'id': '3', 'name': 'Lets learn react'},
+]
+
+
+def home(request):
+    rooms =  Room.objects.all()
+    context = {'rooms': rooms}
+    return render(request, 'base/home.html', context)
+
+
+def room(request, pk):
+    room = Room.objects.get(id=pk)
+    context = {'room': room}
+    return render(request, 'base/room.html', context)
