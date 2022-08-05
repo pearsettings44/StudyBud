7a464c89fe9ebf65365e541f8b3c4f784ef33530
pearsettings44 <jose.a.pereira03@gmail.com>
2022-08-05 16:52:08 +0100

    Chat messages CRUD

--- base/models.py
+++ base/models.py
@@ -3,6 +3,7 @@ from django.contrib.auth.models import User
 
 # Create your models here.
 
+
 class Topic(models.Model):
     name = models.CharField(max_length=200)
 
@@ -10,16 +11,15 @@ class Topic(models.Model):
         return self.name
 
 
-
 class Room(models.Model):
     host = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
     topic = models.ForeignKey(Topic, on_delete=models.SET_NULL, null=True)
     name = models.CharField(max_length=200)
     description = models.TextField(null=True, blank=True)
-    # participants =
+    participants = models.ManyToManyField(
+        User, related_name='participants', blank=True)
     updated = models.DateTimeField(auto_now=True)
     created = models.DateTimeField(auto_now_add=True)
-
 
     class Meta:
         ordering = ['-updated', '-created']
